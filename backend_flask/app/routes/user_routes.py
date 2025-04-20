f70fdc03055608e9d4516aa87f76ae330aa88f6d
Chaabane2k03 <chaabaneboussadia@gmail.com>
2025-04-20 01:53:50 +0100

    merge

--- backend_flask/app/routes/user_routes.py
+++ backend_flask/app/routes/user_routes.py
@@ -2,6 +2,10 @@ from flask import Blueprint
 
 bp = Blueprint("user_routes", __name__)
 
+@bp.route("/")
+def index():
+    return {"message": "Welcome to the User API!"}
+
 @bp.route("/users")
 def get_users():
     return {"users": ["Alice", "Bob"]}
